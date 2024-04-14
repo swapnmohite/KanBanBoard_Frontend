4e3b81c7f38f866909e37b3957996e81f082661f
swapnilmohite495@gmail.com <swapnilmohite495@gmail.com>
2024-04-14 13:55:06 +0530

    changed base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://kanbanboard-tagname.onrender.com",
 });
 
 api.interceptors.request.use(
