17782a48344cfd6878360b1d4bad3fa7f3a8b4c5
swapnilmohite495@gmail.com <swapnilmohite495@gmail.com>
2024-04-19 07:46:16 +0530

    Added Redirection after login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useNavigate,
+} from "react-router-dom";
 import Home from "./Dashboard/Home.jsx";
 import Dashboard from "./Dashboard/Dashboard.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
-const App = () => {
+const MainRoutes = () => {
+    const navigate = useNavigate();
     const isAuthenticated = localStorage.getItem("jwtToken") !== null;
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate("/home");
+        }
+    }, [isAuthenticated]);
+
+    return (
+        <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route
+                path="/home"
+                element={
+                    <ProtectedRoute isAuthenticated={isAuthenticated}>
+                        <Home />
+                    </ProtectedRoute>
+                }
+            />
+        </Routes>
+    );
+};
+
+const App = () => {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route
-                    path="/home"
-                    element={
-                        <ProtectedRoute isAuthenticated={isAuthenticated}>
-                            <Home />
-                        </ProtectedRoute>
-                    }
-                />
-            </Routes>
+            <MainRoutes />
         </Router>
     );
 };
